b3376da016e71e404f030e8f0f7da01c4206be90
Artur <artmame@gmail.com>
2023-01-02 11:08:30 -0500

    Revert change, add note

--- artroom_helpers/gpu_detect.py
+++ artroom_helpers/gpu_detect.py
@@ -1,11 +1,15 @@
 import torch
 
+#Need to expand to include more options, 10XX, 20XX, 30XX for auto xformers
 def is_16xx_series():
-    try:
+    if torch.cuda.is_available():
         gpu_info = torch.cuda.get_device_name(0)
         if '1630' in gpu_info or '1650' in gpu_info or '1660' in gpu_info or '1600' in gpu_info:
             print(gpu_info + ' identified, forcing to full precision')
             return '16XX'
         return 'NVIDIA'
-    except:
+    else:
+        print("Cuda not available.")
+        print("If you are using NVIDIA GPU please try updating your drivers")
+        print("If you are using AMD, it is not yet supported.")
         return 'None'
