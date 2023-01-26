fixed precision issues

stable-diffusion/optimizedSD/ldmv2/modules/diffusionmodules/openaimodel.py
@@ -2,7 +2,7 @@ from abc import abstractmethod
 import math
 
 import numpy as np
-import torch as th
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -23,25 +23,25 @@ from ldmv2.util import exists
 def convert_module_to_f16(x):
     pass
 
+
 def convert_module_to_f32(x):
     pass
 
 
-## go
 class AttentionPool2d(nn.Module):
     """
     Adapted from CLIP: https://github.com/openai/CLIP/blob/main/clip/model.py
     """
 
     def __init__(
-        self,
-        spacial_dim: int,
-        embed_dim: int,
-        num_heads_channels: int,
-        output_dim: int = None,
+            self,
+            spacial_dim: int,
+            embed_dim: int,
+            num_heads_channels: int,
+            output_dim: int = None,
     ):
         super().__init__()
-        self.positional_embedding = nn.Parameter(th.randn(embed_dim, spacial_dim ** 2 + 1) / embed_dim ** 0.5)
+        self.positional_embedding = nn.Parameter(torch.randn(embed_dim, spacial_dim ** 2 + 1) / embed_dim ** 0.5)
         self.qkv_proj = conv_nd(1, embed_dim, 3 * embed_dim, 1)
         self.c_proj = conv_nd(1, embed_dim, output_dim or embed_dim, 1)
         self.num_heads = embed_dim // num_heads_channels
@@ -50,7 +50,7 @@ class AttentionPool2d(nn.Module):
     def forward(self, x):
         b, c, *_spatial = x.shape
         x = x.reshape(b, c, -1)  # NC(HW)
-        x = th.cat([x.mean(dim=-1, keepdim=True), x], dim=-1)  # NC(HW+1)
+        x = torch.cat([x.mean(dim=-1, keepdim=True), x], dim=-1)  # NC(HW+1)
         x = x + self.positional_embedding[None, :, :].to(x.dtype)  # NC(HW+1)
         x = self.qkv_proj(x)
         x = self.attention(x)
@@ -117,16 +117,18 @@ class Upsample(nn.Module):
             x = self.conv(x)
         return x
 
+
 class TransposedUpsample(nn.Module):
     'Learned 2x upsampling without padding'
+
     def __init__(self, channels, out_channels=None, ks=5):
         super().__init__()
         self.channels = channels
         self.out_channels = out_channels or channels
 
-        self.up = nn.ConvTranspose2d(self.channels,self.out_channels,kernel_size=ks,stride=2)
+        self.up = nn.ConvTranspose2d(self.channels, self.out_channels, kernel_size=ks, stride=2)
 
-    def forward(self,x):
+    def forward(self, x):
         return self.up(x)
 
 
@@ -139,7 +141,7 @@ class Downsample(nn.Module):
                  downsampling occurs in the inner-two dimensions.
     """
 
-    def __init__(self, channels, use_conv, dims=2, out_channels=None,padding=1):
+    def __init__(self, channels, use_conv, dims=2, out_channels=None, padding=1):
         super().__init__()
         self.channels = channels
         self.out_channels = out_channels or channels
@@ -176,17 +178,17 @@ class ResBlock(TimestepBlock):
     """
 
     def __init__(
-        self,
-        channels,
-        emb_channels,
-        dropout,
-        out_channels=None,
-        use_conv=False,
-        use_scale_shift_norm=False,
-        dims=2,
-        use_checkpoint=False,
-        up=False,
-        down=False,
+            self,
+            channels,
+            emb_channels,
+            dropout,
+            out_channels=None,
+            use_conv=False,
+            use_scale_shift_norm=False,
+            dims=2,
+            use_checkpoint=False,
+            up=False,
+            down=False,
     ):
         super().__init__()
         self.channels = channels
@@ -250,7 +252,6 @@ class ResBlock(TimestepBlock):
             self._forward, (x, emb), self.parameters(), self.use_checkpoint
         )
 
-
     def _forward(self, x, emb):
         if self.updown:
             in_rest, in_conv = self.in_layers[:-1], self.in_layers[-1]
@@ -265,7 +266,7 @@ class ResBlock(TimestepBlock):
             emb_out = emb_out[..., None]
         if self.use_scale_shift_norm:
             out_norm, out_rest = self.out_layers[0], self.out_layers[1:]
-            scale, shift = th.chunk(emb_out, 2, dim=1)
+            scale, shift = torch.chunk(emb_out, 2, dim=1)
             h = out_norm(h) * (1 + scale) + shift
             h = out_rest(h)
         else:
@@ -282,12 +283,12 @@ class AttentionBlock(nn.Module):
     """
 
     def __init__(
-        self,
-        channels,
-        num_heads=1,
-        num_head_channels=-1,
-        use_checkpoint=False,
-        use_new_attention_order=False,
+            self,
+            channels,
+            num_heads=1,
+            num_head_channels=-1,
+            use_checkpoint=False,
+            use_new_attention_order=False,
     ):
         super().__init__()
         self.channels = channels
@@ -295,7 +296,7 @@ class AttentionBlock(nn.Module):
             self.num_heads = num_heads
         else:
             assert (
-                channels % num_head_channels == 0
+                    channels % num_head_channels == 0
             ), f"q,k,v channels {channels} is not divisible by num_head_channels {num_head_channels}"
             self.num_heads = channels // num_head_channels
         self.use_checkpoint = use_checkpoint
@@ -311,8 +312,9 @@ class AttentionBlock(nn.Module):
         self.proj_out = zero_module(conv_nd(1, channels, channels, 1))
 
     def forward(self, x):
-        return checkpoint(self._forward, (x,), self.parameters(), True)   # TODO: check checkpoint usage, is True # TODO: fix the .half call!!!
-        #return pt_checkpoint(self._forward, x)  # pytorch
+        return checkpoint(self._forward, (x,), self.parameters(),
+                          True)  # TODO: check checkpoint usage, is True # TODO: fix the .half call!!!
+        # return pt_checkpoint(self._forward, x)  # pytorch
 
     def _forward(self, x):
         b, c, *spatial = x.shape
@@ -340,7 +342,7 @@ def count_flops_attn(model, _x, y):
     # The first computes the weight matrix, the second computes
     # the combination of the value vectors.
     matmul_ops = 2 * b * (num_spatial ** 2) * c
-    model.total_ops += th.DoubleTensor([matmul_ops])
+    model.total_ops += torch.DoubleTensor([matmul_ops])
 
 
 class QKVAttentionLegacy(nn.Module):
@@ -362,12 +364,12 @@ class QKVAttentionLegacy(nn.Module):
         assert width % (3 * self.n_heads) == 0
         ch = width // (3 * self.n_heads)
         q, k, v = qkv.reshape(bs * self.n_heads, ch * 3, length).split(ch, dim=1)
-        scale = 1 / math.sqrt(math.sqrt(ch))
-        weight = th.einsum(
+        scale = 1 / matorch.sqrt(matorch.sqrt(ch))
+        weight = torch.einsum(
             "bct,bcs->bts", q * scale, k * scale
         )  # More stable with f16 than dividing afterwards
-        weight = th.softmax(weight.float(), dim=-1).type(weight.dtype)
-        a = th.einsum("bts,bcs->bct", weight, v)
+        weight = torch.softmax(weight.float(), dim=-1).type(weight.dtype)
+        a = torch.einsum("bts,bcs->bct", weight, v)
         return a.reshape(bs, -1, length)
 
     @staticmethod
@@ -394,14 +396,14 @@ class QKVAttention(nn.Module):
         assert width % (3 * self.n_heads) == 0
         ch = width // (3 * self.n_heads)
         q, k, v = qkv.chunk(3, dim=1)
-        scale = 1 / math.sqrt(math.sqrt(ch))
-        weight = th.einsum(
+        scale = 1 / matorch.sqrt(matorch.sqrt(ch))
+        weight = torch.einsum(
             "bct,bcs->bts",
             (q * scale).view(bs * self.n_heads, ch, length),
             (k * scale).view(bs * self.n_heads, ch, length),
         )  # More stable with f16 than dividing afterwards
-        weight = th.softmax(weight.float(), dim=-1).type(weight.dtype)
-        a = th.einsum("bts,bcs->bct", weight, v.reshape(bs * self.n_heads, ch, length))
+        weight = torch.softmax(weight.float(), dim=-1).type(weight.dtype)
+        a = torch.einsum("bts,bcs->bct", weight, v.reshape(bs * self.n_heads, ch, length))
         return a.reshape(bs, -1, length)
 
     @staticmethod
@@ -440,35 +442,35 @@ class UNetModel(nn.Module):
     """
 
     def __init__(
-        self,
-        image_size,
-        in_channels,
-        model_channels,
-        out_channels,
-        num_res_blocks,
-        attention_resolutions,
-        dropout=0,
-        channel_mult=(1, 2, 4, 8),
-        conv_resample=True,
-        dims=2,
-        num_classes=None,
-        use_checkpoint=False,
-        use_fp16=False,
-        num_heads=-1,
-        num_head_channels=-1,
-        num_heads_upsample=-1,
-        use_scale_shift_norm=False,
-        resblock_updown=False,
-        use_new_attention_order=False,
-        use_spatial_transformer=False,    # custom transformer support
-        transformer_depth=1,              # custom transformer support
-        context_dim=None,                 # custom transformer support
-        n_embed=None,                     # custom support for prediction of discrete ids into codebook of first stage vq model
-        legacy=True,
-        disable_self_attentions=None,
-        num_attention_blocks=None,
-        disable_middle_self_attn=False,
-        use_linear_in_transformer=False,
+            self,
+            image_size,
+            in_channels,
+            model_channels,
+            out_channels,
+            num_res_blocks,
+            attention_resolutions,
+            dropout=0,
+            channel_mult=(1, 2, 4, 8),
+            conv_resample=True,
+            dims=2,
+            num_classes=None,
+            use_checkpoint=False,
+            use_fp16=False,
+            num_heads=-1,
+            num_head_channels=-1,
+            num_heads_upsample=-1,
+            use_scale_shift_norm=False,
+            resblock_updown=False,
+            use_new_attention_order=False,
+            use_spatial_transformer=False,  # custom transformer support
+            transformer_depth=1,  # custom transformer support
+            context_dim=None,  # custom transformer support
+            n_embed=None,  # custom support for prediction of discrete ids into codebook of first stage vq model
+            legacy=True,
+            disable_self_attentions=None,
+            num_attention_blocks=None,
+            disable_middle_self_attn=False,
+            use_linear_in_transformer=False,
     ):
         super().__init__()
         if use_spatial_transformer:
@@ -505,7 +507,8 @@ class UNetModel(nn.Module):
             assert len(disable_self_attentions) == len(channel_mult)
         if num_attention_blocks is not None:
             assert len(num_attention_blocks) == len(self.num_res_blocks)
-            assert all(map(lambda i: self.num_res_blocks[i] >= num_attention_blocks[i], range(len(num_attention_blocks))))
+            assert all(
+                map(lambda i: self.num_res_blocks[i] >= num_attention_blocks[i], range(len(num_attention_blocks))))
             print(f"Constructor of UNetModel received num_attention_blocks={num_attention_blocks}. "
                   f"This option has LESS priority than attention_resolutions {attention_resolutions}, "
                   f"i.e., in cases where num_attention_blocks[i] > 0 but 2**i not in attention_resolutions, "
@@ -517,7 +520,7 @@ class UNetModel(nn.Module):
         self.conv_resample = conv_resample
         self.num_classes = num_classes
         self.use_checkpoint = use_checkpoint
-        self.dtype = th.float16 if use_fp16 else th.float32
+        self.dtype = torch.float16 if use_fp16 else torch.float32
         self.num_heads = num_heads
         self.num_head_channels = num_head_channels
         self.num_heads_upsample = num_heads_upsample
@@ -571,7 +574,7 @@ class UNetModel(nn.Module):
                         num_heads = ch // num_head_channels
                         dim_head = num_head_channels
                     if legacy:
-                        #num_heads = 1
+                        # num_heads = 1
                         dim_head = ch // num_heads if use_spatial_transformer else num_head_channels
                     if exists(disable_self_attentions):
                         disabled_sa = disable_self_attentions[level]
@@ -626,7 +629,7 @@ class UNetModel(nn.Module):
             num_heads = ch // num_head_channels
             dim_head = num_head_channels
         if legacy:
-            #num_heads = 1
+            # num_heads = 1
             dim_head = ch // num_heads if use_spatial_transformer else num_head_channels
         self.middle_block = TimestepEmbedSequential(
             ResBlock(
@@ -644,10 +647,10 @@ class UNetModel(nn.Module):
                 num_head_channels=dim_head,
                 use_new_attention_order=use_new_attention_order,
             ) if not use_spatial_transformer else SpatialTransformer(  # always uses a self-attn
-                            ch, num_heads, dim_head, depth=transformer_depth, context_dim=context_dim,
-                            disable_self_attn=disable_middle_self_attn, use_linear=use_linear_in_transformer,
-                            use_checkpoint=use_checkpoint
-                        ),
+                ch, num_heads, dim_head, depth=transformer_depth, context_dim=context_dim,
+                disable_self_attn=disable_middle_self_attn, use_linear=use_linear_in_transformer,
+                use_checkpoint=use_checkpoint
+            ),
             ResBlock(
                 ch,
                 time_embed_dim,
@@ -682,7 +685,7 @@ class UNetModel(nn.Module):
                         num_heads = ch // num_head_channels
                         dim_head = num_head_channels
                     if legacy:
-                        #num_heads = 1
+                        # num_heads = 1
                         dim_head = ch // num_heads if use_spatial_transformer else num_head_channels
                     if exists(disable_self_attentions):
                         disabled_sa = disable_self_attentions[level]
@@ -730,10 +733,10 @@ class UNetModel(nn.Module):
         )
         if self.predict_codebook_ids:
             self.id_predictor = nn.Sequential(
-            normalization(ch),
-            conv_nd(dims, model_channels, n_embed, 1),
-            #nn.LogSoftmax(dim=1)  # change to cross_entropy and produce non-normalized logits
-        )
+                normalization(ch),
+                conv_nd(dims, model_channels, n_embed, 1),
+                # nn.LogSoftmax(dim=1)  # change to cross_entropy and produce non-normalized logits
+            )
 
     def convert_to_fp16(self):
         """
@@ -751,7 +754,7 @@ class UNetModel(nn.Module):
         self.middle_block.apply(convert_module_to_f32)
         self.output_blocks.apply(convert_module_to_f32)
 
-    def forward(self, x, timesteps=None, context=None, y=None,**kwargs):
+    def forward(self, x, timesteps=None, context=None, y=None, **kwargs):
         """
         Apply the model to an input batch.
         :param x: an [N x C x ...] Tensor of inputs.
@@ -761,7 +764,7 @@ class UNetModel(nn.Module):
         :return: an [N x C x ...] Tensor of outputs.
         """
         assert (y is not None) == (
-            self.num_classes is not None
+                self.num_classes is not None
         ), "must specify y if and only if the model is class-conditional"
         hs = []
         t_emb = timestep_embedding(timesteps, self.model_channels, repeat_only=False)
@@ -777,7 +780,7 @@ class UNetModel(nn.Module):
             hs.append(h)
         h = self.middle_block(h, emb, context)
         for module in self.output_blocks:
-            h = th.cat([h, hs.pop()], dim=1)
+            h = torch.cat([h, hs.pop()], dim=1)
             h = module(h, emb, context)
         h = h.type(x.dtype)
         if self.predict_codebook_ids:

stable_diffusion.py
@@ -27,6 +27,7 @@ from artroom_helpers import support, inpainting
 
 sys.path.append("stable-diffusion/optimizedSD")
 from ldm.util import instantiate_from_config
+
 logging.set_verbosity_error()
 
 warnings.filterwarnings("ignore", category=DeprecationWarning)
@@ -52,6 +53,7 @@ def load_model_from_config(ckpt, use_safe_load=True):
     if "state_dict" in pl_sd:
         return pl_sd["state_dict"]
     return pl_sd
+
 
 def load_mask(mask, newH, newW):
     image = np.array(mask)
@@ -96,8 +98,10 @@ def load_img(image, h0, w0, inpainting=False):
     image = torch.from_numpy(image)
     return 2. * image - 1.
 
+
 class StableDiffusion:
-    def __init__(self, socketio=None, Upscaler = None):
+    def __init__(self, socketio=None, Upscaler=None):
+        self.dtype = None
         self.Upscaler = Upscaler
 
         self.current_num = 0
@@ -108,7 +112,7 @@ class StableDiffusion:
         self.artroom_path = None
 
         self.model = None
-        self.modelCS = None 
+        self.modelCS = None
         self.modelFS = None
 
         self.ckpt = ''
@@ -123,8 +127,8 @@ class StableDiffusion:
         self.v1 = False
         self.cc = self.get_cc()
 
-        #Generation Runtime Parameters
-        
+        # Generation Runtime Parameters
+
     def get_cc(self):
         try:
             cc = torch.cuda.get_device_capability()
@@ -175,7 +179,7 @@ class StableDiffusion:
     def load_vae(self, vae_path, safe_load_=True):
         vae = load_model_from_config(vae_path, safe_load_)
         vae = {k: v for k, v in vae.items() if
-        
+
                ("loss" not in k) and
                (k not in ['quant_conv.weight', 'quant_conv.bias', 'post_quant_conv.weight',
                           'post_quant_conv.bias'])}
@@ -248,8 +252,6 @@ class StableDiffusion:
             self.modelCS = self.model  # just link without a copy
             self.modelFS = self.model  # just link without a copy
             self.v1 = False
-
-
         else:
             self.v1 = True
             if sd['model.diffusion_model.input_blocks.0.0.weight'].shape[1] == 9:
@@ -322,6 +324,10 @@ class StableDiffusion:
             self.modelCS.half()
             self.modelFS.half()
             torch.set_default_tensor_type(torch.HalfTensor)
+        else:
+            self.model.to(torch.float32)
+            self.modelCS.to(torch.float32)
+            self.modelFS.to(torch.float32)
         del sd
         self.ckpt = ckpt.replace(os.sep, '/')
         self.speed = speed
@@ -338,7 +344,7 @@ class StableDiffusion:
 
     def get_image(self, init_image_str, mask_b64):
         if len(init_image_str) == 0:
-            return None 
+            return None
 
         if init_image_str[:4] == 'data':
             print("Loading image from b64")
@@ -367,7 +373,7 @@ class StableDiffusion:
         image = np.array(image).astype(np.float32) / 255.0
         image = image[None].transpose(0, 3, 1, 2)
         image = torch.from_numpy(image)
-        init_image =  2. * image - 1.
+        init_image = 2. * image - 1.
         init_image = init_image.to(self.device)
         _, _, H, W = image.shape
         if self.is_nvidia:
@@ -378,8 +384,10 @@ class StableDiffusion:
     def generate(self, text_prompts="", negative_prompts="", batch_name="", init_image_str="", mask_b64="",
                  invert=False,
                  steps=50, H=512, W=512, strength=0.75, cfg_scale=7.5, seed=-1, sampler="ddim", C=4, ddim_eta=0.0, f=8,
-                 n_iter=4, batch_size=1, ckpt="", vae="", image_save_path="", speed="High", skip_grid=False, batch_id=0):
+                 n_iter=4, batch_size=1, ckpt="", vae="", image_save_path="", speed="High", skip_grid=False,
+                 batch_id=0):
         self.running = True
+        self.dtype = torch.float16 if self.is_nvidia else torch.float32
 
         if batch_id == 0:
             batch_id = random.randint(1, 922337203685)
@@ -435,8 +443,7 @@ class StableDiffusion:
 
             init_image = load_img(image.convert('RGB'), H, W, inpainting=(len(mask_b64) > 0)).to(self.device)
             _, _, H, W = init_image.shape
-            if self.is_nvidia:
-                init_image = init_image.half()
+            init_image = init_image.to(self.dtype)
         else:
             init_image = None
 
@@ -462,14 +469,9 @@ class StableDiffusion:
             if steps <= 0:
                 steps = 1
 
-        if self.is_nvidia:
-            precision_scope = autocast
-        else:
-            precision_scope = nullcontext
-
         self.total_num = n_iter
         all_samples = []
-
+        precision_scope = autocast if self.is_nvidia else nullcontext
         with torch.no_grad():
             for n in range(n_iter):
                 if not self.running:
@@ -505,9 +507,9 @@ class StableDiffusion:
                                 weight = weighted_prompt[i][1]
                                 # if not skip_normalize:
                                 c = torch.add(c, self.modelCS.get_learned_conditioning(
-                                    weighted_prompt[i][0]), alpha=weight)
+                                    weighted_prompt[i][0]), alpha=weight).to(self.device)
                         else:
-                            c = self.modelCS.get_learned_conditioning(prompts)
+                            c = self.modelCS.get_learned_conditioning(prompts).to(self.device)
                         shape = [batch_size, C, H // f, W // f]
 
                         print("Sampler", sampler)
@@ -515,8 +517,6 @@ class StableDiffusion:
                         for ij in range(1, highres_fix_steps + 1):
                             if init_image is not None:
                                 init_image = init_image.to(self.device)
-                                if self.is_nvidia:
-                                    init_image = init_image.half()
                                 init_image = repeat(
                                     init_image, '1 ... -> b ...', b=batch_size)
                                 init_latent = self.modelFS.get_first_stage_encoding(
@@ -540,7 +540,8 @@ class StableDiffusion:
                                 if invert:
                                     mask_image = ImageOps.invert(mask_image)
 
-                                mask = load_mask(mask_image, init_latent.shape[2], init_latent.shape[3]).to(self.device)
+                                mask = load_mask(mask_image, init_latent.shape[2], init_latent.shape[3])\
+                                    .to(self.device)
                                 mask = mask[0][0].unsqueeze(0).repeat(4, 1, 1).unsqueeze(0)
                                 mask = repeat(mask, '1 ... -> b ...', b=batch_size)
                                 if self.v1:
@@ -577,6 +578,7 @@ class StableDiffusion:
                                 (x_samples_ddim + 1.0) / 2.0, min=0.0, max=1.0)
                             # print(x_sample.shape)  # 1, 3, 768, 768
                             if abs(float(x_sample.flatten().sum())) <= 1.0:  # black square
+                                print("Fixing a black square")
                                 self.modelFS.to(torch.float32)
                                 x_samples_ddim = self.modelFS.decode_first_stage(
                                     x0[0].to(torch.float32).unsqueeze(0))
@@ -590,50 +592,48 @@ class StableDiffusion:
                                 x_sample.astype(np.uint8))
                             if ij < highres_fix_steps - 1:
                                 init_image = load_img(
-                                    out_image, H * (ij + 1), W * (ij + 1))
-                                if self.is_nvidia:
-                                    init_image = init_image.half()
+                                    out_image, H * (ij + 1), W * (ij + 1)).to(self.device).to(self.dtype)
                             elif ij == highres_fix_steps - 1:
-                                init_image = load_img(out_image, oldH, oldW)
-                                if self.is_nvidia:
-                                    init_image = init_image.half()
+                                init_image = load_img(out_image, oldH, oldW).to(self.device).to(self.dtype)
 
-                        if mask is not None:
-                            if init_image_str[:4] == 'data':
-                                original_init_image = support.b64_to_image(init_image_str).convert('RGB')
-                            else:
-                                original_init_image = Image.open(init_image_str).convert('RGB')
+                            if mask is not None:
+                                if init_image_str[:4] == 'data':
+                                    original_init_image = support.b64_to_image(init_image_str).convert('RGB')
+                                else:
+                                    original_init_image = Image.open(init_image_str).convert('RGB')
 
-                            out_image = support.repaste_and_color_correct(result=out_image,
-                                                                          init_image=original_init_image,
-                                                                          init_mask=mask_image, mask_blur_radius=8)
+                                out_image = support.repaste_and_color_correct(result=out_image,
+                                                                              init_image=original_init_image,
+                                                                              init_mask=mask_image, mask_blur_radius=8)
 
-                        exif_data = out_image.getexif()
-                        # Does not include Mask, ImageB64, or if Inverted. Only settings for now
-                        settings_data = {
-                            "text_prompts": text_prompts,
-                            "negative_prompts": negative_prompts,
-                            "steps": steps,
-                            "H": H,
-                            "W": W,
-                            "strength": strength,
-                            "cfg_scale": cfg_scale,
-                            "seed": seed,
-                            "sampler": sampler,
-                            "ckpt": os.path.basename(ckpt),
-                            "vae": os.path.basename(vae)
-                        }
-                        # 0x9286 Exif Code for UserComment
-                        exif_data[0x9286] = json.dumps(settings_data)
-                        out_image.save(
-                            os.path.join(sample_path, save_name), "JPEG", exif=exif_data)
+                            exif_data = out_image.getexif()
+                            # Does not include Mask, ImageB64, or if Inverted. Only settings for now
+                            settings_data = {
+                                "text_prompts": text_prompts,
+                                "negative_prompts": negative_prompts,
+                                "steps": steps,
+                                "H": H,
+                                "W": W,
+                                "strength": strength,
+                                "cfg_scale": cfg_scale,
+                                "seed": seed,
+                                "sampler": sampler,
+                                "ckpt": os.path.basename(ckpt),
+                                "vae": os.path.basename(vae)
+                            }
+                            # 0x9286 Exif Code for UserComment
+                            exif_data[0x9286] = json.dumps(settings_data)
+                            out_image.save(
+                                os.path.join(sample_path, save_name), "JPEG", exif=exif_data)
 
-                        self.socketio.emit('get_images', {'b64': support.image_to_b64(out_image), 'path': os.path.join(sample_path, save_name), 'batch_id': batch_id })
+                            self.socketio.emit('get_images', {'b64': support.image_to_b64(out_image),
+                                                              'path': os.path.join(sample_path, save_name),
+                                                              'batch_id': batch_id})
 
-                        base_count += 1
-                        seed += 1
-                        if not skip_grid and n_iter > 1:
-                            all_samples.append(out_image)
+                            base_count += 1
+                            seed += 1
+                            if not skip_grid and n_iter > 1:
+                                all_samples.append(out_image)
 
             if not skip_grid and n_iter > 1:
                 # additionally, save as grid