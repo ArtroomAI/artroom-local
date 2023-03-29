728e8ae47c69992aa81b488a84856d0a9f01a195
Bartosz Leoniak <leoniak.bartosz99@gmail.com>
2023-03-29 17:36:17 +0200

    fix downloading models, refactor code

--- src/main/ipcHandles.ts
+++ src/main/ipcHandles.ts
@@ -7,20 +7,64 @@ import yauzl from "yauzl";
 
 let installationProcess: ChildProcessWithoutNullStreams;
 
-async function removeDirectoryIfExists(PATH: fs.PathLike) {
-    try {
-      const exists = fs.existsSync(PATH);
-      if (exists) {
-        console.log(`${PATH} exists! Deleting...`)
-        await fs.promises.rmdir(PATH, { recursive: true });
-        console.log(`${PATH} deleted`)
-      }
-    } catch (error) {
-      console.error(error);
+const toMB = (n: number) => (n / 1048576).toFixed(2); //1048576 - bytes in 1 MB
+
+function removeDirectoryIfExists(PATH: fs.PathLike) {
+  try {
+    const exists = fs.existsSync(PATH);
+    if (exists) {
+      console.log(`${PATH} exists! Deleting...`)
+      fs.rmdirSync(PATH, { recursive: true });
+      console.log(`${PATH} deleted`)
     }
+  } catch (error) {
+    console.error(error);
   }
+}
+
+function download_via_https(URL: string, file_path: string, mainWindow: Electron.BrowserWindow) {
+  return new Promise<boolean>((resolve) => {
+    const request = https.get(URL, (response) => {
+      const len = parseInt(response.headers['content-length'], 10);
+      let cur = 0;
+
+      const file = fs.createWriteStream(file_path);
+      response.pipe(file);
+
+      const total = toMB(len);
+
+      let chunk_counter = 0;
+
+      response.on("data", (chunk) => {
+        cur += chunk.length;
+        ++chunk_counter;
+        if(chunk_counter === 5000) {
+          console.log(`Downloading ${(100 * cur / len).toFixed(2)}% - ${toMB(cur)}mb / ${total}mb`);
+          mainWindow.webContents.send('fixButtonProgress', `Downloading ${(100 * cur / len).toFixed(2)}% - ${toMB(cur)}mb / ${total}mb`);
+          chunk_counter = 0;
+        }
+      });
+
+      file.on("finish", () => {
+        file.close();
+        resolve(true);
+      });
+
+      file.on("error", () => {
+        file.close();
+        resolve(false);
+      })
+      
+      request.on("error", (e) => {
+        mainWindow.webContents.send('fixButtonProgress', `Error: ${e.message}`);
+        file.close();
+        resolve(false);
+      });
+    });
+  });
+}
   
-const backupPythonInstallation = (mainWindow: Electron.BrowserWindow, artroomPath: string, gpuType: string) => () => {
+const backupPythonInstallation = async (mainWindow: Electron.BrowserWindow, artroomPath: string, gpuType: string) => {
     console.log("REINSTALL BACKEND")
     console.log(`VANILLA PATH: ${artroomPath}`)
     const URL = gpuType === 'AMD' ? 
@@ -38,108 +82,81 @@ const backupPythonInstallation = (mainWindow: Electron.BrowserWindow, artroomPat
 
     const installationCommand = `"${PATH}/Scripts/conda" run --no-capture-output -p "${PATH}/envs/artroom-ldm" python -m pip install -r "${PATH_requirements}" && set /p choice= "Finished! Please exit out of this window or press enter to close"`;
 
-    removeDirectoryIfExists(PATH).then(()=>{
-        const request = https.get(URL, (response) => {
-            if (fs.existsSync(PATH_zip)) {
-              fs.unlinkSync(PATH_zip);
+    removeDirectoryIfExists(PATH);
+
+    if (fs.existsSync(PATH_zip)) {
+      fs.unlinkSync(PATH_zip);
+    }
+  
+    fs.mkdirSync(path.join(artroomPath, "artroom", "settings"), { recursive: true });
+
+    const success = await download_via_https(URL, PATH_zip, mainWindow);
+
+    if(!success) return;
+
+    yauzl.open(PATH_zip, { lazyEntries: true }, (error, zipFile) => {
+      if (error) {
+        console.error(`Error opening ZIP archive: ${error}`);
+        return;
+      }
+    
+      const totalEntries = zipFile.entryCount;
+      let extractedEntries = 0;
+    
+      zipFile.readEntry();
+    
+      zipFile.on("entry", (entry) => {
+        if (/\/$/.test(entry.fileName)) {
+          // Directory entry
+          fs.mkdirSync(`${artroomPath}/artroom/${entry.fileName}`, { recursive: true });
+          zipFile.readEntry();
+        } else {
+          // File entry
+          zipFile.openReadStream(entry, (error, readStream) => {
+            if (error) {
+              console.error(`Error opening read stream for ${entry.fileName}: ${error}`);
+              zipFile.readEntry();
+              return;
             }
-            fs.mkdirSync(path.join(artroomPath, "artroom", "settings"), { recursive: true });
-
-            const len = parseInt(response.headers['content-length'], 10);
-            let cur = 0;
-            const toMB = (n: number) => (n / 1048576).toFixed(2);
     
-            const file = fs.createWriteStream(PATH_zip);
-            response.pipe(file);
+            const writeStream = fs.createWriteStream(`${artroomPath}/artroom/${entry.fileName}`);
     
-            const total = toMB(len); //1048576 - bytes in 1 MB
+            writeStream.on("close", () => {
+              extractedEntries++;
     
-            let chunk_counter = 0;
-    
-            response.on("data", (chunk) => {
-                cur += chunk.length;
-                ++chunk_counter;
-                if(chunk_counter === 5000) {
-                    console.log(`Downloading ${(100 * cur / len).toFixed(2)}% - ${toMB(cur)}mb / ${total}mb`);
-                    mainWindow.webContents.send('fixButtonProgress', `Downloading ${(100 * cur / len).toFixed(2)}% - ${toMB(cur)}mb / ${total}mb`);
-                    chunk_counter = 0;
-                }
-           });
-    
-            file.on("finish", () => {
-                file.close();
-                yauzl.open(PATH_zip, { lazyEntries: true }, (error, zipFile) => {
-                  if (error) {
-                    console.error(`Error opening ZIP archive: ${error}`);
-                    return;
-                  }
-                
-                  const totalEntries = zipFile.entryCount;
-                  let extractedEntries = 0;
-                
-                  zipFile.readEntry();
-                
-                  zipFile.on("entry", (entry) => {
-                    if (/\/$/.test(entry.fileName)) {
-                      // Directory entry
-                      fs.mkdirSync(`${artroomPath}/artroom/${entry.fileName}`, { recursive: true });
-                      zipFile.readEntry();
-                    } else {
-                      // File entry
-                      zipFile.openReadStream(entry, (error, readStream) => {
-                        if (error) {
-                          console.error(`Error opening read stream for ${entry.fileName}: ${error}`);
-                          zipFile.readEntry();
-                          return;
-                        }
-                
-                        const writeStream = fs.createWriteStream(`${artroomPath}/artroom/${entry.fileName}`);
-                
-                        writeStream.on("close", () => {
-                          extractedEntries++;
-                
-                          const progress = Math.round((extractedEntries / totalEntries) * 100);
-                          console.log();
-                          mainWindow.webContents.send('fixButtonProgress', `Extracting... ${progress}%`);
-                          zipFile.readEntry();
-                        });
-                
-                        readStream.pipe(writeStream);
-                      });
-                    }
-                  });
-                
-                  zipFile.on("error", (error) => {
-                    mainWindow.webContents.send('fixButtonProgress', `Error reading ZIP archive: ${error}`);
-                  });
-                
-                  zipFile.on("end", () => {
-                    mainWindow.webContents.send('fixButtonProgress', "Extraction complete. Updating packages...");
-                    // Delete the ZIP file
-                    fs.unlinkSync(PATH_zip);
-                    installationProcess = spawn(installationCommand, { shell: true, detached: true });
-                    installationProcess.stdout.on("data", (data) => {
-                        console.log(`stdout: ${data}`);
-                        mainWindow.webContents.send('fixButtonProgress', `Finished! Please try reopening the app`);
-                    });
-                    installationProcess.stderr.on("data", (data) => {
-                        console.error(`stderr: ${data}`);
-                    });
-                    installationProcess.on("close", (code) => {
-                        console.log(`child process exited with code ${code}`);
-                        mainWindow.webContents.send('fixButtonProgress', `Finished! Please try reopening the app`);
-                    });
-                  });
-                });
+              const progress = Math.round((extractedEntries / totalEntries) * 100);
+              console.log();
+              mainWindow.webContents.send('fixButtonProgress', `Extracting... ${progress}%`);
+              zipFile.readEntry();
             });
     
-            request.on("error", (e) => {
-                mainWindow.webContents.send('fixButtonProgress', `Error: ${e.message}`);
-                file.close();
-            });
+            readStream.pipe(writeStream);
+          });
+        }
+      });
+    
+      zipFile.on("error", (error) => {
+        mainWindow.webContents.send('fixButtonProgress', `Error reading ZIP archive: ${error}`);
+      });
+    
+      zipFile.on("end", () => {
+        mainWindow.webContents.send('fixButtonProgress', "Extraction complete. Updating packages...");
+        // Delete the ZIP file
+        fs.unlinkSync(PATH_zip);
+        installationProcess = spawn(installationCommand, { shell: true, detached: true });
+        installationProcess.stdout.on("data", (data) => {
+          console.log(`stdout: ${data}`);
+          mainWindow.webContents.send('fixButtonProgress', `Finished! Please try reopening the app`);
         });
-    })
-
+        installationProcess.stderr.on("data", (data) => {
+          console.error(`stderr: ${data}`);
+        });
+        installationProcess.on("close", (code) => {
+          console.log(`child process exited with code ${code}`);
+          mainWindow.webContents.send('fixButtonProgress', `Finished! Please try reopening the app`);
+        });
+      });
+    });
 };
 
 const reinstallPythonDependencies = (artroomPath: string) => () => {
@@ -151,29 +168,29 @@ const reinstallPythonDependencies = (artroomPath: string) => () => {
     installationProcess = spawn(installationCommand, { shell: true, detached: true });
 
     installationProcess.stdout.on('data', function(data) {
-        console.log("Child data: " + data);
-      });
-      installationProcess.on('error', function () {
-        console.log("Failed to start child.");
-      });
-      installationProcess.on('close', function (code) {
-        console.log('Child process exited with code ' + code);
-      });
-      installationProcess.stderr.on('data', function (err) {
-        console.log(`error: ${err}`);
-      });
-      installationProcess.on('message', (msg) => {
-        console.log(`msg ${msg}`)
-      })
-      installationProcess.stderr.on('message', (msg) => {
-        console.log(`ermsg ${msg}`)
-      })
-      installationProcess.stdout.on('end', function () {
-        console.log('Finished collecting data chunks.');        
-      });
+      console.log("Child data: " + data);
+    });
+    installationProcess.on('error', function () {
+      console.log("Failed to start child.");
+    });
+    installationProcess.on('close', function (code) {
+      console.log('Child process exited with code ' + code);
+    });
+    installationProcess.stderr.on('data', function (err) {
+      console.log(`error: ${err}`);
+    });
+    installationProcess.on('message', (msg) => {
+      console.log(`msg ${msg}`)
+    })
+    installationProcess.stderr.on('message', (msg) => {
+      console.log(`ermsg ${msg}`)
+    })
+    installationProcess.stdout.on('end', function () {
+      console.log('Finished collecting data chunks.');        
+    });
 }
 
-const downloadStarterModels = (mainWindow: Electron.BrowserWindow, dir: string, realisticStarter: boolean, animeStarter: boolean, landscapesStarter: boolean) => () => {
+const downloadStarterModels = async (mainWindow: Electron.BrowserWindow, dir: string, realisticStarter: boolean, animeStarter: boolean, landscapesStarter: boolean) => {
   fs.mkdirSync(dir, { recursive: true });
 
   const bucketPath = "https://pub-060d7c8cf5e64af8b884ebb86d34de1a.r2.dev/models/models/"
@@ -181,75 +198,37 @@ const downloadStarterModels = (mainWindow: Electron.BrowserWindow, dir: string, 
   const animeModel = "UmiAIMythologyAndBabes_macrossAnimeUltimate1.safetensors"
   const landscapesModel = "UmiAIMythologyAndBabes_macrossAnimeUltimate1.safetensors"
 
-  const realisticURL = bucketPath + realisticModel;
-  const animeURL = bucketPath + animeModel;
-  const landscapesURL = bucketPath + landscapesModel;
+  const realisticURL = path.join(bucketPath, realisticModel);
+  const animeURL = path.join(bucketPath, animeModel);
+  const landscapesURL = path.join(bucketPath, landscapesModel);
 
-  const downloadModel = (modelURL: string, callback: () => void) => {
-    https.get(modelURL, (response) => {
+  const realisticPath = path.join(dir, realisticModel);
+  const animePath = path.join(dir, animeModel);
+  const landscapesPath = path.join(dir, landscapesModel);
 
-      const len = parseInt(response.headers['content-length'], 10);
-      let cur = 0;
-      const toMB = (n: number) => (n / 1048576).toFixed(2);
-
-      const file = fs.createWriteStream(path.join());
-      response.pipe(file);
-
-      const total = toMB(len); //1048576 - bytes in 1 MB
-
-      let chunk_counter = 0;
-
-      response.on("data", (chunk) => {
-          cur += chunk.length;
-          ++chunk_counter;
-          if(chunk_counter === 3000) {
-              console.log(`Downloading ${(100 * cur / len).toFixed(2)}% - ${toMB(cur)}mb / ${total}mb`);
-              mainWindow.webContents.send('fixButtonProgress', `Downloading ${(100 * cur / len).toFixed(2)}% - ${toMB(cur)}mb / ${total}mb`);
-              chunk_counter = 0;
-          }
-      });
-
-      file.on("finish", () => {
-          file.close();
-          callback();
-      });
-
-      response.on("error", (e) => {
-          mainWindow.webContents.send('fixButtonProgress', `Error: ${e.message}`);
-          file.close();
-          callback();
-      });
-    })
+  if (realisticStarter) {
+    console.log(`DOWNLOAINDG FROM ${realisticURL}`)
+    await download_via_https(realisticURL, realisticPath, mainWindow);
   }
-
-  const downloadAllModels = async () => {
-    if (realisticStarter) {
-      console.log(`DOWNLOAINDG FROM ${realisticURL}`)
-      await new Promise<void>((resolve) => downloadModel(realisticURL, resolve));
-    }
-    if (animeStarter) {
-      console.log(`DOWNLOAINDG FROM ${animeStarter}`)
-      await new Promise<void>((resolve) => downloadModel(animeURL, resolve));
-    }
-    if (landscapesStarter) {
-      console.log(`DOWNLOAINDG FROM ${landscapesStarter}`)
-      await new Promise<void>((resolve) => downloadModel(landscapesURL, resolve));
-    }
-    console.log("All downloads complete!");
-  };
-
-  downloadAllModels();
+  if (animeStarter) {
+    console.log(`DOWNLOAINDG FROM ${animeStarter}`)
+    await download_via_https(animeURL, animePath, mainWindow);
+  }
+  if (landscapesStarter) {
+    console.log(`DOWNLOAINDG FROM ${landscapesStarter}`)
+    await download_via_https(landscapesURL, landscapesPath, mainWindow);
+  }
+  console.log("All downloads complete!");
 }
-
 
 export const handlers = (mainWindow: Electron.BrowserWindow) => {
   ipcMain.handle('pythonInstall', (event, artroomPath, gpuType) => {
-    backupPythonInstallation(mainWindow, artroomPath, gpuType)();
+    backupPythonInstallation(mainWindow, artroomPath, gpuType);
   });    
   ipcMain.handle('pythonInstallDependencies', (event, artroomPath) => {
     reinstallPythonDependencies(artroomPath)();
   });    
   ipcMain.handle('downloadStarterModels', (event, dir, realisticStarter, animeStarter, landscapesStarter) => {
-    downloadStarterModels(mainWindow, dir, realisticStarter, animeStarter, landscapesStarter)();
+    downloadStarterModels(mainWindow, dir, realisticStarter, animeStarter, landscapesStarter);
   });    
 }
