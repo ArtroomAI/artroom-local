625dca475a117861a9010b8954d1f6c53ee16069
artmamedov <artmame@gmail.com>
2023-04-02 15:02:40 -0400

    v1.1.2-beta.23
    Remove Tour

--- src/renderer/components/Sidebar.tsx
+++ src/renderer/components/Sidebar.tsx
@@ -109,7 +109,7 @@ export default function Sidebar () {
 
                     <Discord />
 
-                    <Tour />
+                    {/* <Tour /> */}
 
                     <NavItem
                         className="settings-nav"
