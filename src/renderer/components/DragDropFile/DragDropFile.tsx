64755e919b81724d8d661ced8def4a54b002daad
Bartosz Leoniak <leoniak.bartosz99@gmail.com>
2023-05-02 01:39:55 +0200

    clean up and optimise Body, Paint (mask painting)

--- src/renderer/components/DragDropFile/DragDropFile.tsx
+++ src/renderer/components/DragDropFile/DragDropFile.tsx
@@ -1,6 +1,6 @@
-import React, { useCallback, useEffect, useRef } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { useRecoilState, useSetRecoilState } from 'recoil';
-import * as atom from '../../atoms/atoms';
+import { initImagePathState, aspectRatioSelectionState } from '../../atoms/atoms';
 import {
     Box,
     Image as ChakraImage,
@@ -16,21 +16,27 @@ import {
     FaTrashAlt, FaClipboardList, FaQuestionCircle
 } from 'react-icons/fa';
 import { aspectRatioState, initImageState } from '../../SettingsManager';
-import { getImageDimensions } from '../Utils/getImageDimensions';
+import { getImageDimensions } from '../Utils/image';
+import { useDropzone } from 'react-dropzone';
 
 const DragDropFile = () => {
-    const inputRef = useRef<HTMLInputElement>(null);
-    const [initImagePath, setInitImagePath] = useRecoilState(atom.initImagePathState);
-    const [aspectRatioSelection, setAspectRatioSelection] = useRecoilState(atom.aspectRatioSelectionState);
+    const [initImagePath, setInitImagePath] = useRecoilState(initImagePathState);
+    const [aspectRatioSelection, setAspectRatioSelection] = useRecoilState(aspectRatioSelectionState);
     const [initImage, setInitImage] = useRecoilState(initImageState);
     const setAspectRatio = useSetRecoilState(aspectRatioState);
+
+    const onDrop = useCallback((acceptedFiles: File[]) => {
+        handleFile(acceptedFiles[0]);
+    }, []);
+
+    const { getRootProps, getInputProps, open } = useDropzone({ onDrop, useFsAccessApi: false });
 
     useEffect(() => {
         if (initImagePath) {
             console.log(initImagePath);
-            window.api.getImageFromPath(initImagePath).then((result) => {
-                getImageDimensions(result.b64).then(({ width, height }) => {
-                    setInitImage(result.b64);
+            window.api.getImageFromPath(initImagePath).then(({ b64 }) => {
+                getImageDimensions(b64).then(({ width, height }) => {
+                    setInitImage(b64);
                     if (aspectRatioSelection === "Init Image") {
                         setAspectRatio(`${width}:${height}`);
                     }
@@ -41,21 +47,6 @@ const DragDropFile = () => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
       }, [initImagePath]);
 
-    // Handle drag events
-    const handleDrag: React.DragEventHandler<HTMLElement> = function (e) {
-        e.preventDefault();
-        e.stopPropagation();
-    };
-
-    // Triggers when file is dropped
-    const handleDrop: React.DragEventHandler<HTMLDivElement> = useCallback((e) => {
-        e.preventDefault();
-        e.stopPropagation();
-        if (e.dataTransfer.files && e.dataTransfer.files[0]) {
-            handleFile(e.dataTransfer.files[0]);
-        }
-    }, []);
-
     const handleFile = useCallback((file: File) => {
         if (file.type === "image/jpeg" || file.type === "image/png" || file.type === "image/heic") {
             console.log(file.path);
@@ -65,99 +56,51 @@ const DragDropFile = () => {
         }
     }, []);
 
-    // Triggers when file is selected with click
-    const handleChange: React.ChangeEventHandler<HTMLInputElement> = function (e) {
-        e.preventDefault();
-        if (e.target.files && e.target.files[0]) {
-            handleFile(e.target.files[0]);
-        }
-    };
-
-    // Triggers the input when the button is clicked
-    const onButtonClick = () => {
-        inputRef.current.click();
-    };
-
     return (
         <VStack>
             <Box
                 bg="#080B16"
                 height="180px"
                 width="180px"
+                {...getRootProps()}
             >
-                {initImage.length > 0
-                    ? <Box
-                        border="1px"
-                        borderStyle="ridge"
-                        height="180px"
-                        onClick={onButtonClick}
-                        onDragEnter={handleDrag}
-                        onDragLeave={handleDrag}
-                        onDragOver={handleDrag}
-                        onDrop={handleDrop}
-                        rounded="md"
-                        style={{ display: 'flex',
-                            alignItems: 'center',
-                            justifyContent: 'center',
-                            textAlign: 'center',
-                            borderColor: '#FFFFFF20' }}
-                        width="180px"
-                    >
+                <Box
+                    border="1px"
+                    borderStyle="ridge"
+                    height="180px"
+                    rounded="md"
+                    style={{ display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        textAlign: 'center',
+                        borderColor: '#FFFFFF20' }}
+                    width="180px"
+                >
+                    {initImage.length > 0 ?
                         <ChakraImage
                             boxSize="178px"
                             fit="contain"
                             rounded="md"
                             src={initImage}
                         />
-                    </Box>
-                    : <Box
-                        border="1px"
-                        borderStyle="ridge"
-                        height="180px"
-                        onClick={onButtonClick}
-                        onDragEnter={handleDrag}
-                        onDragLeave={handleDrag}
-                        onDragOver={handleDrag}
-                        onDrop={handleDrop}
-                        px={4}
-                        rounded="md"
-                        style={{ display: 'flex',
-                            alignItems: 'center',
-                            justifyContent: 'center',
-                            textAlign: 'center',
-                            borderColor: '#FFFFFF20' }}
-                        width="180px"
-                    >
+                        : <>
                             Click or Drag A Starting Image Here
-                        <Tooltip
-                            fontSize="md"
-                            label="Upload an image to use as the starting point for your generation instead of just random noise"
-                            placement="top"
-                            shouldWrapChildren>
-                            <FaQuestionCircle color="#777" />
-                        </Tooltip>
-                    </Box> }
-
-                <form
-                    id="form-file-upload"
-                    onDragEnter={handleDrag}
-                    onSubmit={(e) => e.preventDefault()}
-                >
-                    <input
-                        accept="image/png, image/jpeg"
-                        id="input-file-upload"
-                        multiple={false}
-                        onChange={handleChange}
-                        ref={inputRef}
-                        type="file"
+                            <Tooltip
+                                fontSize="md"
+                                label="Upload an image to use as the starting point for your generation instead of just random noise"
+                                placement="top"
+                                shouldWrapChildren>
+                                <FaQuestionCircle color="#777" />
+                            </Tooltip>
+                        </>
+                    }
+                </Box>
+                <input
+                    {...getInputProps({
+                        id: "input-file-upload",
+                        accept: "image/png, image/jpeg",
+                        "multiple": false })}
                     />
-
-                    <label
-                        htmlFor="input-file-upload"
-                        id="label-file-upload"
-                    >
-                    </label>
-                </form>
             </Box>
             <ButtonGroup width="100%" isAttached variant="outline">
                 <Tooltip label="Paste from Clipboard">
@@ -167,20 +110,26 @@ const DragDropFile = () => {
                         border="2px"
                         icon={<FaClipboardList />}
                         onClick={() => {
-                        navigator.clipboard.read().then((data) => {
-                            for (let i = 0; i < data.length; i++) {
-                                if (data[i].types.includes('image/png') || data[i].types.includes('image/jpeg')) {
-                                    data[i].getType('image/png').then((blob) => {
+                            navigator.clipboard.read().then((data) => {
+                                const clip = data[0];
+                                if(clip && (clip.types.includes('image/png') || clip.types.includes('image/jpeg'))) {
+                                    clip.getType('image/png').then((blob) => {
                                         const reader = new FileReader();
                                         reader.readAsDataURL(blob);
                                         reader.onloadend = () => {
                                             const base64data = reader.result;
-                                            setInitImage(base64data);
+                                            if(base64data === null) {
+                                                return;
+                                            }
+                                            if(typeof base64data === 'string') {
+                                                setInitImage(base64data);
+                                            } else {
+                                                const enc = new TextDecoder("utf-8");
+                                                setInitImage(enc.decode(new Uint8Array(base64data)));
+                                            }
                                         };
                                     });
-                                    break;
                                 }
-                            }
                             });
                         }}
                     />
@@ -189,31 +138,29 @@ const DragDropFile = () => {
                     <IconButton
                         border="2px"
                         icon={<FiUpload />}
-                        onClick={onButtonClick}
+                        onClick={open}
                         width="90px"
                         aria-label="upload"
                     />
                 </Tooltip>
                 <Tooltip label="Clear Starting Image">
                     <IconButton
-                    aria-label="Clear Starting Image"
-                    border="2px"
-                    icon={<FaTrashAlt />}
-                    width="45px"
-                    onClick={() => {
-                        inputRef.current.value = null;
-                        setInitImagePath("");
-                        setInitImage("");
-                        if (aspectRatioSelection === "Init Image") {
-                        setAspectRatio("None");
-                        setAspectRatioSelection("None");
-                        }
-                    }}
+                        aria-label="Clear Starting Image"
+                        border="2px"
+                        icon={<FaTrashAlt />}
+                        width="45px"
+                        onClick={() => {
+                            setInitImagePath("");
+                            setInitImage("");
+                            if (aspectRatioSelection === "Init Image") {
+                                setAspectRatio("None");
+                                setAspectRatioSelection("None");
+                            }
+                        }}
                     />
                 </Tooltip>
             </ButtonGroup>
         </VStack>
-
     );
 };
 
--- src/renderer/components/Utils/getImageDimensions.ts
+++
@@ -1,6 +0,0 @@
-export const getImageDimensions = async (base64: string) => {
-    const img = new Image();
-    img.src = base64;
-    await img.decode();
-    return { width: img.naturalWidth, height: img.naturalHeight };
-};
